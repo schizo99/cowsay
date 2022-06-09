e05da84ddf698c23743cc61d35381d3184c3e20e
schizo99 <schizo99@gmail.com>
2022-06-09 08:14:40 +0200

    Make it possible to change worker_class

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
 threads = int(os.environ.get('GUNICORN_THREADS', '2'))
+worker_class = os.environ.get('GUNICORN_WORKER_CLASS', "gthread")
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
